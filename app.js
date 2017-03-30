408f1c04c79b69da2b8b7c1fc6d49216ab07e92f
Ramon Castan <rcastan@br.ibm.com>
2017-03-30 15:28:01 -0300

    CRUD ops

--- app.js
+++ app.js
@@ -1,72 +1,107 @@
 /* app.js */
-var PORT = 3000;
-const app = require('express')()
+const app = require('express')();
 
-const posts = [
-    {
-        id: 1,
-        author: 'Edi',
-        title: 'Templates',
-        body: 'Blog post number 1'
-    },
-    {
-        id: 2,
-        author: 'Carol',
-        title: 'Another set of templates',
-        body: 'Blog number 2'
-    }
-];
+var cloudant = require('cloudant')('https://16a38da7-1ac7-4202-b583-e9689da37bfb-bluemix:f1841b46949345eab60c77cd65ca9ee161fefffc27f1356da00f995b197b6a6d@16a38da7-1ac7-4202-b583-e9689da37bfb-bluemix.cloudant.com');
 
-const locations = [
-    {
-        city: 'SaoPaulo',
-        stores: 2
-    },
-    {
-        city: 'Campinas',
-        stores: 1
-    }
-];
+cloudant = cloudant.db.use('posts');
 
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true })); 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 
+var posts;
+
 // home page
 app.get('/', (req,res) => {
-     res.render('home', {posts: posts})
-})
+    cloudant.find({
+	 	"selector":{
+	 		"_id":{"$gt":null}
+	 	},
+	 	"fields":[
+	 	"id","author","title","body"
+	 	]
+	 },function(err,data){
+	 	if(!err){
+            posts = data.docs;
+	 		res.render('home',{posts:posts});
+	 		return;
+	 	}		
+	 	console.log(err);
+	 });
+     
+});
 
-// blog post
+app.get('/create',(req,res) => {    
+    res.render('create-post');
+    
+});
 
-app.get('/post/:id', (req,res) => {
-    const post = posts.filter((post) => {
-        return post.id == req.params.id
-    })[0]
 
-// render post.ejs
-    res.render('post', {
+app.post('/create',(req,res) => {
+    console.log(req.body);  
+    res.render('create-post');
+    cloudant.insert(req.body,function(err,body){
+       if(!err) console.log(body); 
+    });
+});
+
+
+app.get('/post/:title', (req,res) => {
+    var title = req.params.title;
+     cloudant.find({
+	 	"selector":{
+	 		"title":{"$eq":title}
+	 	},
+	 	"fields":[
+	 	"id","author","title","body"
+	 	]
+	 },function(err,data){
+	 	if(!err){
+            var post = data.docs[0];
+            // render post.ejs
+            res.render('post', {
                            author: post.author,
                            title: post.title,
                            body: post.body
                            })
-})
+	 		return;
+	 	}		
+	 	console.log(err);
+	 });
+});
 
-// blog locations
-app.get('/store', (req,res) => {
-    res.render('store', {locations: locations})
-})
-
-app.get('/locations/:city', (req,res) => {
-    const locations = locations.filter((locations) => {
-        return locations.city == req.params.city
-    })[0]
-
-// render blog locations
-    res.render('locations', {
-        city: locations.city,
-        stores: locations.stores
-    })
+app.post('/delete', (req,res) => {
     
-})
-app.listen(PORT);
+    var title = req.body.title;
+    
+    cloudant.find({
+	 	"selector":{
+	 		"title":{"$eq":title}
+	 	},
+	 	"fields":[
+	 	"_id","_rev"
+	 	]
+	 },function(err,data){
+	 	if(!err){
+            var doc_id = data.docs[0]._id;
+            var doc_rev = data.docs[0]._rev;
+            
+            cloudant.destroy(doc_id,doc_rev,function(err){
+                if(!err) console.log("document deleted");
+                
+            });
+            
+	 		return;
+	 	}		
+	 	console.log(err);
+	 });
+    
+});
+
+
+app.listen(3000);
 console.log("Listening on PORT 3000");
+
